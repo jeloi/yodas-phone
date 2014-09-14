fdf4224992b290c1cb97fd581e1a9d7f4d0a1019
Jeloi <jeremyho19@gmail.com>
2014-09-13 23:26:43 -0700

    move routes to routes.js. setup twilio restclient.

--- app.js
+++ app.js
@@ -4,10 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var twilio = require('twilio');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./routes');
 
 var app = express();
 
@@ -24,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
