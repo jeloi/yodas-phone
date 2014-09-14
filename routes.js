d8c2f5717fafdffabc28f616f272bbe5947bc1ea
Jeloi <jeremyho19@gmail.com>
2014-09-14 01:18:03 -0700

    add basic twiml to routes

--- routes.js
+++ routes.js
@@ -3,16 +3,27 @@ var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
 
-// App Configs
-var configs = require('./configs');
+// App config
+var config = require('./config');
 // Twilio Client
-var client = new twilio.RestClient(configs.twilio.accountSid, configs.twilio.authToken);
+var client = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/texts', function(req, res) {
+	if (twilio.validateExpressRequest(req, config.twilio.authToken)) {
+	    var twiml = new twilio.TwimlResponse();
+	    twiml.say('express sez - hello twilio!');
 
+	    res.type('text/xml');
+	    res.send(twiml.toString());
+	}
+	else {
+	    res.send('you are not twilio.  Buzz off.');
+	}
+})
 
 module.exports = router;
