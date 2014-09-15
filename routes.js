95bbdf7d80a3efc63e6dfdd4cce2335a4d73ea95
Jeloi <jeremyho19@gmail.com>
2014-09-14 21:26:02 -0700

    add yoda-speak module (linked) and add response

--- routes.js
+++ routes.js
@@ -2,11 +2,15 @@
 var express = require('express');
 var router = express.Router();
 var twilio = require('twilio');
+var https = require('https');
+var YodaSpeak = require('yoda-speak');
 
 // App config
 var config = require('./config');
 // Twilio Client
 var client = new twilio.RestClient(config.twilio.accountSid, config.twilio.authToken);
+// Yoda client from Mashape API
+var yoda = new YodaSpeak(config.mashape.apiKey);
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -16,19 +20,26 @@ router.get('/', function(req, res) {
 router.post('/texts', function(req, res) {
 	if (twilio.validateExpressRequest(req, config.twilio.authToken, {url: config.twilio.smsWebhook})) {
 	    var twiml = new twilio.TwimlResponse();
-	    twiml.message('express sez - hello twilio!');
 
 	    // The text sent in
 	    var body = req.param('Body').trim();
-        
-        // the number the vote it being sent to (this should match an Event)
-        var to = req.param('To');
-        
-        // the voter, use this to keep people from voting more than once
-        var from = req.param('From');
 
-	    res.type('text/xml');
-	    res.send(twiml.toString());
+	    yoda.convert(body, function(err, result) {
+	    	if (!err) {
+	    		console.log(result.toString());
+	    		twiml.message = result.toString();
+	    		res.type('text/xml');
+	    		res.send(twiml.toString());
+	    	} else {
+	    		res.status(err.status || 500);
+	    		res.render('error', {
+	    		    message: err.message,
+	    		    error: err
+	    		});
+	    	}
+	    })
+        // var to = req.param('To');
+        // var from = req.param('From');
 	}
 	else {
 	    res.send('Hey! You are not twilio.  Buzz off.');
